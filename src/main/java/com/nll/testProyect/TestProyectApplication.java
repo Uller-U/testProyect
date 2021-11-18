5c5a4b4c4be39e938e83c82a7d8f78108c15d103
NikoLopez <nicolas.lopez@rmesanalytics.com>
2021-11-17 21:39:01 -0300

    test
    test comment

---
+++ src/main/java/com/nll/testProyect/TestProyectApplication.java
@@ -0,0 +1,13 @@
+package com.nll.testProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nll/testProyect/TestProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nll.testProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
